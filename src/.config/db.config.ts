57f2d9a21228d0156f7d2329ae017133ea8ed113
血无痕 <cfy555388com@163.com>
2023-03-14 07:45:39 +0800

    项目构建

--- src/.config/db.config.ts
+++ src/.config/db.config.ts
@@ -1,8 +1,8 @@
 export const MYSQLCONFIG:any = {
   type: "mysql", //数据库类型
   username: "root", //账号
-  password: "799520cfyCFY.", //密码
-  host: "175.178.216.33", //host
+  password: "root", //密码
+  host: "localhost", //host
   port: 3306,
   database: "community", //库名
   synchronize: true, //synchronize字段代表是否自动将实体类同步到数据库
